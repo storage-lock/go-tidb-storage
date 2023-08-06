eaba7854407d3a9d66b1ee8e2f215a3e4981a721
songzhibin97 <718428482@qq.com>
2023-08-06 20:51:58 +0800

    fix: update helper version & script host

--- scripts/tidb.sh
+++ scripts/tidb.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+docker rm -f db-storage-tidb-server
+
 # 启动TiDB实例，默认的用户名为root，密码为空，监听在4000端口
 docker run --name db-storage-tidb-server -d -p 4000:4000 -p 10080:10080 pingcap/tidb:latest
 
-export STORAGE_LOCK_TIDB_DSN="root:@tcp(192.168.128.206:4000)/storage_lock_test"
+export STORAGE_LOCK_TIDB_DSN="root:@tcp(127.0.0.1:4000)/storage_lock_test"
