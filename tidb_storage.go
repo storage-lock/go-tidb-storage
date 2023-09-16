93cd3afa987c414e29ab36c36fd015cb1051c4d8
CC11001100 <CC11001100@qq.com>
2023-09-17 03:55:23 +0800

    基于mysql实现

--- tidb_storage.go
+++ tidb_storage.go
@@ -2,257 +2,77 @@ package tidb_storage
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"time"
-
-	storage_lock "github.com/storage-lock/go-storage-lock"
-
-	"github.com/golang-infrastructure/go-iterator"
+	mysql_storage "github.com/storage-lock/go-mysql-storage"
 	"github.com/storage-lock/go-storage"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type TidbStorage struct {
-	options       *TidbStorageOptions
-	tableFullName string
+	*mysql_storage.MysqlStorage
 }
 
 var _ storage.Storage = &TidbStorage{}
 
 func NewTidbStorage(ctx context.Context, options *TidbStorageOptions) (*TidbStorage, error) {
-	s := &TidbStorage{
-		options: options,
+
+	if err := options.Check(); err != nil {
+		return nil, err
 	}
 
-	err := s.Init(ctx)
+	mysqlStorage, err := mysql_storage.NewMysqlStorage(ctx, options.toMysqlStorageOptions())
 	if err != nil {
 		return nil, err
+	}
+
+	s := &TidbStorage{
+		MysqlStorage: mysqlStorage,
 	}
 
 	return s, nil
 }
 
-const StorageName = "mysql-storage"
+const StorageName = "tidb-storage"
 
 func (x *TidbStorage) GetName() string {
 	return StorageName
 }
 
-func (x *TidbStorage) Init(ctx context.Context) (returnError error) {
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	// TODO 要不要自动创建数据库呢？这是一个值得讨论的问题。
-	// 用户有可能是想把数据库连接放到当前的数据库下，也可能是想放到别的数据库下
-	// 如果想放到别的数据库下，用户应该为其创建专门的数据库
-	// 如果是复用连接的话，则有可能会有需求是切换数据库，也许这里只应该标记一下，作为能够用之后的优化项
-
-	// 创建存储锁信息需要的表
-	// TODO 这个参数后面涉及到多处拼接sql，可能会有sql注入，是否需要做一些安全措施？
-	tableFullName := x.options.TableName
-	if tableFullName == "" {
-		tableFullName = fmt.Sprintf("`%s`.`%s`", storage.DefaultStorageDatabaseName, storage.DefaultStorageTableName)
-	}
-	createTableSql := `CREATE TABLE IF NOT EXISTS %s (
-    lock_id VARCHAR(255) NOT NULL PRIMARY KEY,
-    owner_id VARCHAR(255) NOT NULL,
-    version BIGINT NOT NULL,
-    lock_information_json_string VARCHAR(255) NOT NULL
-)`
-	_, err = db.Exec(fmt.Sprintf(createTableSql, tableFullName))
-	if err != nil {
-		return err
-	}
-
-	x.tableFullName = tableFullName
-
-	return nil
-}
-
-func (x *TidbStorage) UpdateWithVersion(ctx context.Context, lockId string, exceptedVersion, newVersion storage.Version, lockInformation *storage.LockInformation) (returnError error) {
-
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	insertSql := fmt.Sprintf(`UPDATE %s SET version = ?, lock_information_json_string = ? WHERE lock_id = ? AND owner_id = ? AND version = ?`, x.tableFullName)
-	execContext, err := db.ExecContext(ctx, insertSql, newVersion, lockInformation.ToJsonString(), lockId, lockInformation.OwnerId, exceptedVersion)
-	if err != nil {
-		return err
-	}
-	affected, err := execContext.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if affected == 0 {
-		return storage_lock.ErrVersionMiss
-	}
-	return nil
-}
-
-func (x *TidbStorage) CreateWithVersion(ctx context.Context, lockId string, version storage.Version, lockInformation *storage.LockInformation) (returnError error) {
-
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	insertSql := fmt.Sprintf(`INSERT INTO %s (lock_id, owner_id, version, lock_information_json_string) VALUES (?, ?, ?, ?)`, x.tableFullName)
-	execContext, err := db.ExecContext(ctx, insertSql, lockId, lockInformation.OwnerId, version, lockInformation.ToJsonString())
-	if err != nil {
-		return err
-	}
-	affected, err := execContext.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if affected == 0 {
-		return storage_lock.ErrVersionMiss
-	}
-	return nil
-}
-
-func (x *TidbStorage) DeleteWithVersion(ctx context.Context, lockId string, exceptedVersion storage.Version, lockInformation *storage.LockInformation) (returnError error) {
-
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	deleteSql := fmt.Sprintf(`DELETE FROM %s WHERE lock_id = ? AND owner_id = ? AND version = ?`, x.tableFullName)
-	execContext, err := db.ExecContext(ctx, deleteSql, lockId, lockInformation.OwnerId, exceptedVersion)
-	if err != nil {
-		return err
-	}
-	affected, err := execContext.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if affected == 0 {
-		return storage_lock.ErrVersionMiss
-	}
-	return nil
-}
-
-func (x *TidbStorage) Get(ctx context.Context, lockId string) (lockInformationJsonString string, returnError error) {
-
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	getLockSql := fmt.Sprintf("SELECT lock_information_json_string FROM %s WHERE lock_id = ?", x.tableFullName)
-	rs, err := db.Query(getLockSql, lockId)
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		_ = rs.Close()
-	}()
-	if !rs.Next() {
-		return "", storage_lock.ErrLockNotFound
-	}
-	err = rs.Scan(&lockInformationJsonString)
-	if err != nil {
-		return "", err
-	}
-	return lockInformationJsonString, nil
-}
-
-func (x *TidbStorage) GetTime(ctx context.Context) (now time.Time, returnError error) {
-
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return time.Time{}, err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	var zero time.Time
-	// TODO 多实例的情况下可能会有问题，允许其能够比较方便的切换到NTP TimeProvider
-	rs, err := db.Query("SELECT UNIX_TIMESTAMP(NOW())")
-	if err != nil {
-		return zero, err
-	}
-	defer func() {
-		err := rs.Close()
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-	if !rs.Next() {
-		return zero, errors.New("rs server time failed")
-	}
-	var databaseTimestamp uint64
-	err = rs.Scan(&databaseTimestamp)
-	if err != nil {
-		return zero, err
-	}
-
-	// TODO 时区
-	return time.Unix(int64(databaseTimestamp), 0), nil
-}
-
-func (x *TidbStorage) Close(ctx context.Context) error {
-	// 没有Storage级别的资源好回收的
-	return nil
-}
-
-func (x *TidbStorage) List(ctx context.Context) (iterator iterator.Iterator[*storage.LockInformation], returnError error) {
-
-	db, err := x.options.ConnectionManager.Take(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		err := x.options.ConnectionManager.Return(ctx, db)
-		if returnError == nil {
-			returnError = err
-		}
-	}()
-
-	rows, err := db.Query("SELECT * FROM %s", x.tableFullName)
-	if err != nil {
-		return nil, err
-	}
-	return storage.NewSqlRowsIterator(rows), nil
-}
+//func (x *TidbStorage) Init(ctx context.Context) (returnError error) {
+//	db, err := x.options.ConnectionManager.Take(ctx)
+//	if err != nil {
+//		return err
+//	}
+//	defer func() {
+//		err := x.options.ConnectionManager.Return(ctx, db)
+//		if returnError == nil {
+//			returnError = err
+//		}
+//	}()
+//
+//	// TODO 要不要自动创建数据库呢？这是一个值得讨论的问题。
+//	// 用户有可能是想把数据库连接放到当前的数据库下，也可能是想放到别的数据库下
+//	// 如果想放到别的数据库下，用户应该为其创建专门的数据库
+//	// 如果是复用连接的话，则有可能会有需求是切换数据库，也许这里只应该标记一下，作为能够用之后的优化项
+//
+//	// 创建存储锁信息需要的表
+//	// TODO 这个参数后面涉及到多处拼接sql，可能会有sql注入，是否需要做一些安全措施？
+//	tableFullName := x.options.TableName
+//	if tableFullName == "" {
+//		tableFullName = fmt.Sprintf("`%s`.`%s`", storage.DefaultStorageDatabaseName, storage.DefaultStorageTableName)
+//	}
+//	createTableSql := `CREATE TABLE IF NOT EXISTS %s (
+//    lock_id VARCHAR(255) NOT NULL PRIMARY KEY,
+//    owner_id VARCHAR(255) NOT NULL,
+//    version BIGINT NOT NULL,
+//    lock_information_json_string VARCHAR(255) NOT NULL
+//)`
+//	_, err = db.Exec(fmt.Sprintf(createTableSql, tableFullName))
+//	if err != nil {
+//		return err
+//	}
+//
+//	x.tableFullName = tableFullName
+//
+//	return nil
+//}
